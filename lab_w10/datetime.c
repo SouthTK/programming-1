6fe79c0529e3853fdc5743750f0db1e97d4e8ac6
Nam <khanhnam176@gmail.com>
2024-12-03 07:29:28 +0000

    ok

--- lab_w10/datetime.c
+++ lab_w10/datetime.c
@@ -1,20 +1,21 @@
 #include <time.h>
+#include <stdio.h>
 
 void sprint_time(char* outstr, struct tm* info)
 {
-
+    sprintf(outstr, "Current time is: %02d:%02d:%02d",  info->tm_hour, info->tm_min, info->tm_sec);
 }
 
 void sprint_date(char* outstr, struct tm* info)
 {
-
+    sprintf(outstr, "Today is: Wed, %d/%d/%d", info->tm_mday, info->tm_mon + 1, info->tm_year + 1900);
 }
 
 int main()
 {
     time_t current_time = time(NULL);
     struct tm* info = localtime(&current_time);
-
+    char outstr[50];
     // print current time
     info->tm_hour; // hour
     info->tm_min; // minute
@@ -26,6 +27,9 @@ int main()
     info->tm_year + 1900; // year
 
     info->tm_wday; // day of the week: 0 -> Sun, 1 -> Mon, ...
-
+    sprint_time(outstr, info);
+    printf("%s \n", outstr);
+    sprint_date(outstr, info);
+    printf("%s ", outstr);
     return 0;
 }
--- lab_w10/datetime_input.c
+++ lab_w10/datetime_input.c
@@ -3,12 +3,13 @@
 int main()
 {
     printf("Enter the date with following format \"Weekday dd/mm/yyyy\" \n");
-
     char weekday[10];
     int day, month, year;
-
     scanf("%s %d/%d/%d", weekday, &day, &month, &year);
-
-    printf("Your entered date: %s, %.2d/%.2d/%.4d", weekday, day, month, year);
+    printf("Enter the time with following format \"hh:mm:ss\" \n");
+    int hour, minute, second;
+    scanf("%d:%d:%d", &hour, &minute, &second);
+    printf("Your entered date: %s, %.02d/%02d/%04d \n", weekday, day, month, year);
+    printf("Your entered time: %02d:%02d:%02d", hour, minute, second);
     return 0;
 }
